2f14bd8c83fa38d81c20f635dba54a9c2239f8d3
skatt931 <i.kurnytskyi@gmail.com>
2017-01-09 12:18:33 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,55 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    browserSync  = require('browser-sync'),
-    imagemin     = require('gulp-imagemin'),
-    pngquant     = require('imagemin-pngquant'),
-    cache        = require('gulp-cache'),
-    autoprefixer = require('gulp-autoprefixer');
+var gulp 			= require('gulp'),
+	sass 			= require('gulp-sass'),
+	browserSync 	= require('browser-sync'),
+	cssnano     	= require('gulp-cssnano'), 
+    rename      	= require('gulp-rename'); 
 
-gulp.task('sass', function(){
-    return gulp.src('app/sass/**/*.scss')
-        .pipe(sass()) //
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({stream: true}))
+gulp.task('sass', function () {
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+  	.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app/'
+gulp.task('browser-sync', function() { 
+    browserSync({ 
+        server: { 
+            baseDir: 'app' // directory app
         },
-        notify: false
+        notify: false // notify off
     });
 });
 
-gulp.task('watch', ['browser-sync'], function() {
-    gulp.watch('app/sass/**/*.scss', ['sass']);
-    gulp.watch('app/*.html', browserSync.reload);
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('app/css/main.css') 
+        .pipe(cssnano()) // Minify
+        .pipe(rename({suffix: '.min'})) // Додаємо суфікс min
+        .pipe(gulp.dest('app/css')); 
 });
 
-gulp.task('img', function() {
-    var imgSrc = './app/img/**/*',
-        imgDst = './dist/img';
-
-    return gulp.src(imgSrc)
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDst));
+gulp.task('watch', ['browser-sync', 'css-libs', 'sass'], function() {
+	gulp.watch('sass/**/*.scss',['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
 
-gulp.task('build', ['img', 'sass'], function() {
+gulp.task('build', ['sass'], function() {
 
-    var buildCss = gulp.src([
-        'app/css/main.css'
-    ])
-        .pipe(gulp.dest('dist/css'))
+    var buildCss = gulp.src([ 
+        'app/css/main.css',
+        'app/css/main.min.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') 
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') 
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') 
+    .pipe(gulp.dest('dist'));
+
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch']); //Default task now is 'watch'
